ac71af93e00990371c85cff4399f1b70ded60fa3
Phil S <philipstubbs13@gmail.com>
2018-03-28 19:33:17 -0500

    set up server.js file. Got server running.

---
+++ public/assets/app.js
@@ -0,0 +1,11 @@
+$.getJSON("/all", function(data) {
+    //For each entry of that json...
+    console.log(data);
+    for (var i =0; i < data.length; i++) {
+
+    }
+})
+
+$("#savearticle").on("click", function() {
+    
+})
---
+++ server.js
@@ -0,0 +1,65 @@
+//Dependencies
+var express = require("express");
+var mongojs = require("mongojs");
+
+//Initialize Express
+var app = express();
+
+//Set up a static folder (public) for our web app.
+app.use(express.static("public"));
+
+//Database configuration
+//Save the URL of our database as well as the name of our collection.
+var databaseURL = "news";
+var collections = ["articles"];
+
+//Use mongojs to hook the database to the db variable.
+var db = mongojs(databaseURL, collections);
+
+
+//This akes sure that any errors are logged if mongodb runs into an issue.
+db.on("error", function(error) {
+    console.log("Database Error", error);
+});
+
+//Routes
+//1. At the root path, send a simple hello world message to the browser.
+app.get("/", function(req, res) {
+    res.send("Hello world");
+});
+
+//2. At the "/all" path, display every entry in the articles collection.
+app.get("/all", function(req, res) {
+    //Query: in our database, go to the articles collection, then "find" everything.
+    db.articles.find({}, function(error, found) {
+        //Log any errors if the server encounters one.
+        if (error) {
+            console.log(error);
+        }
+        //Otherwise, send the result of this query to the browser.
+        else {
+            res.json(found);
+        }
+    });
+});
+
+//3. At the "/name" path, display every entry in the articles collection, sorted by name.
+app.get("/name", function(req, res) {
+    //Query: In our database, go to the articles collection, then "find" everything.
+    //But this time, sort it by name (1 means ascending order)
+    db.articles.find().sort({ name: 1}, function (error, found) {
+        //Log any errors if the server encounters one.
+        if (error) {
+            console.log(error);
+        }
+        //Otherwise, send the result of this query to the browser.
+        else {
+            res.json(found);
+        }
+    });
+});
+
+//Set the app to listen on port 3000
+app.listen(3000, function() {
+    console.log("App running on port 3000");
+})
