752e41c6b0088aed18703464ee3183195ea2abe7
Phil S <philipstubbs13@gmail.com>
2018-04-01 05:28:17 -0500

    Save article and delete article now working.

--- controllers/fetch.js
+++ controllers/fetch.js
@@ -3,6 +3,8 @@ var db = require("../models");
 
 //Require express
 var express = require("express");
+
+//Require mongojs
 var mongojs = require("mongojs");
 
 //Scraping tools
@@ -11,9 +13,10 @@ var axios = require("axios");
 
 module.exports = function(app) {
     //Routes
-    // At the root path, display/render home.handlebars file.
+    //At the root path, display/render home.handlebars file.
     app.get("/", function(req, res) {
-        //Query: in our database, go to the articles collection, then "find" everything.
+        //Query: in our database, go to the headlines collection, 
+        //then "find" every article that is not saved (has a saved value of false).
         db.Headline.find({saved: false}, function(error, found) {
             //Log any errors if the server encounters one.
             if (error) {
@@ -30,7 +33,8 @@ module.exports = function(app) {
 
     //At the /saved path, display/render saved.handlebars file.
     app.get("/saved", function(req, res) {
-        //Query: in our database, go to the articles collection, then "find" everything.
+        //Query: in our database, go to the articles collection, 
+        //then "find" every article that is saved (has a saved value of true);
         db.Headline.find({saved: true}, function(error, found) {
             //Log any errors if the server encounters one.
             if (error) {
@@ -46,7 +50,7 @@ module.exports = function(app) {
     });
 
     //This route will retrieve all of the data.
-    //from the scrapedData collection as a json (this will be populated by the data you scrape using the next route.)
+    //from the headlines collection as json (this will be populated by the data you scrape using the next route.)
     app.get("/all", function(req, res) {
         //Query: in our database, go to the articles collection, then "find" everything.
         db.Headline.find({}, function(error, found) {
@@ -62,28 +66,28 @@ module.exports = function(app) {
     });
 
     //When you visit this route, the server will 
-    //scrape data from the site of your choice, and save it to MongoDB.
-    //TIP: Think back to how you pushed website data
-    //into an empty array in the last class. How do you
-    //push it intto a MongoDB collection instead?
+    //scrape data from the site, and save it to MongoDB.
     app.get("/scrape", function(req, res) {
 
-        // First, we grab the body of the html with request
+        //First, we grab the body of the html with axios.
         axios.get("http://www.espn.com/mens-college-basketball/").then(function(response) {
-            // Then, we load that into cheerio and save it to $ for a shorthand selector
+            //Then, we load that into cheerio and save it to $ for a shorthand selector
             var $ = cheerio.load(response.data)
 
+            //For each element that has a class of .contentitem__content on the site that we are trying to scrape, do the following...
             $(".contentItem__content").each(function(i, element){
                 //Save an empty result object.
                 var result = {};
+                //Save the title of each article from the site to the result object as result.title.
                 result.title = $(this).find(".contentItem__title").text();
+                //Save the link for each article from the site to the result object as result.link.
                 result.link = "http://www.espn.com" + $(this).find("a").attr("href");
-                // var date = $(this).find(".post__date").text();
+                //Save the summary for each article from the site to the result object as result.summary.
                 result.summary = $(this).find(".contentItem__subhead").text();
-                // var imgLink = $(element).find("a").find("img").attr("data-srcset").split(",")[0].split(" ")[0];
+                //Save the image/photo associated with each article from the site to the result object as result.img.
                 result.img = $(this).find(".media-wrapper").find(".media-wrapper_image").find("img").attr("data-default-src");
     
-
+                //Create a new article in the database using the result object built from scraping...
                 db.Headline.create(result)
                     .then(function(dbHeadline) {
                         //View the added result in the console.
@@ -95,23 +99,22 @@ module.exports = function(app) {
                     });        
             });
             res.send("Scrape complete");
-            res.json(result);
         });
     });
 
-    // Mark a article as saved.
+    //Mark a article as saved.
     app.put("/marksaved/:id", function(req, res) {
-        // Remember: when searching by an id, the id needs to be passed in
-        // as (mongojs.ObjectId(IDYOUWANTTOFIND))
+        //Remember: when searching by an id, the id needs to be passed in
         updateSaved(true,req, res);
     });
   
-    // Mark an article as not saved.
+    //Mark an article as not saved.
     app.put("/markunsaved/:id", function(req, res) {
-        // Remember: when searching by an id, the id needs to be passed in
+        //Remember: when searching by an id, the id needs to be passed in
         updateSaved(false,req, res);
     });
 
+    //Function that marks an article as saved (saved: true) or not saved (saved: false).
     function updateSaved(isSaved, req, res) {
         db.Headline.findOneAndUpdate({ _id: req.params.id }, { saved: isSaved },
             function(err, data) {
@@ -120,9 +123,22 @@ module.exports = function(app) {
                 }
                 else {
                 res.json(data);
-                res.end();
                 }
         });
     }
+
+    //Delete an article
+    app.delete("/articles/:id", function(req, res) {
+        // Remember: when searching by an id, the id needs to be passed in
+        db.Headline.deleteOne({ _id: req.params.id },
+            function(err, data) {
+                if (err) {
+                    console.log(err);
+                }
+                else {
+                res.json(data);
+                }
+        });
+    });
 
 }
--- public/assets/app.js
+++ public/assets/app.js
@@ -2,35 +2,56 @@
 //Click event for "Save article" button.
 $(".save-article-btn").on("click", function(event) {
     console.log("save article button clicked");
-  // Grab the id associated with the article.
-  var thisId = $(this).attr("data-id");
+    //Grab the id associated with the article.
+    var thisId = $(this).attr("data-id");
 
-  // Run a PUT request to update saved value in db.
-  $.ajax({
-    method: "PUT",
-    url: "/marksaved/" + thisId,
-  })
-    // With that done
+    //Run a PUT request to update saved value from false to true in the database.
+    $.ajax({
+        method: "PUT",
+        url: "/marksaved/" + thisId,
+    })
+    //With that done
     .then(function(data) {
-      // Log the response
+      //Log the response
       console.log(data);
+      //Reload the page to see the updated list of atricles.
+      location.reload();
     });
 });
 
 //Click event for "Remove from saved" button.
 $(".remove-saved-btn").on("click", function(event) {
     console.log("remove saved button clicked");
-  // Grab the id associated with the article.
-  var thisId = $(this).attr("data-id");
+    //Grab the id associated with the article.
+    var thisId = $(this).attr("data-id");
 
-  // Run a PUT request to update saved value in db.
-  $.ajax({
-    method: "PUT",
-    url: "/markunsaved/" + thisId
-  })
-    // With that done
+    //Run a PUT request to update saved value from true to false in the database.
+    $.ajax({
+        method: "PUT",
+        url: "/markunsaved/" + thisId
+    })
+    //With that done
     .then(function(data) {
-      // Log the response
-      console.log(data);
+        //Log the response
+        console.log(data);
+        //Reload the page to get the updated list of articles.
+        location.reload();
     });
 });
+
+//Click event to delete an article.
+$(".delete-article-btn").on("click", function(event) {
+    console.log("delete button clicked");
+    var id = $(this).data("id");
+
+    // Send the DELETE request using ajax.
+    $.ajax("/articles/" + id, {
+        type: "DELETE",
+    }).then(
+        function() {
+        console.log("deleted article", id);
+        // Reload the page to get the updated list of articles.
+        location.reload();
+        }
+    );
+    });
