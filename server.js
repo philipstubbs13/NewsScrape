08d3939b5281b49de12a42692e53b4f5e249a729
Phil S <philipstubbs13@gmail.com>
2018-04-01 23:27:48 -0500

    minor updates to style

--- server.js
+++ server.js
@@ -22,37 +22,11 @@ app.use(logger("dev"));
 //Set up a static folder (public) for our web app.
 app.use(express.static("public"));
 
-// // If deployed, use the deployed database. Otherwise use the local mongoHeadlines database
-// var MONGODB_URI = process.env.MONGODB_URI || "mongodb://ds127129.mlab.com:27129/heroku_1kjtsvrd";
-// //mongodb://ds127129.mlab.com:27129/heroku_1kjtsvrd
-// //mongodb://localhost/newsscraper
-
-
-// // By default mongoose uses callbacks for async queries, we're setting it to use promises (.then syntax) instead
-// // Connect to the Mongo DB
-// mongoose.Promise = Promise;
-// mongoose.connect(MONGODB_URI, {});
-
 //setting up the database
-// const config = require('./config/database');
 mongoose.Promise = Promise;
-// mongoose
-//   .connect(config.database)
-//   .then( result => {
-//     console.log(`Connected to database '${result.connections[0].name}' on ${result.connections[0].host}:${result.connections[0].port}`)
-//   })
-//   .catch(err => console.log('There was an error with your connection:', err));
-
-  //https://stackoverflow.com/questions/45667535/why-heroku-kills-my-process-with-status-143?rq=1
-//   var reqTimer = setTimeout(function wakeUp() {
-//     request("https://hoops-scraper.herokuapp.com", function() {
-//        console.log("WAKE UP DYNO");
-//     });
-//     return reqTimer = setTimeout(wakeUp, 1200000);
-//  }, 1200000);
 
  // Mongoose (orm) connects to our mongo db and allows us to have access to the MongoDB commands for easy CRUD 
-// If deployed, use the deployed database. Otherwise use the local mongoHeadlines database
+// If deployed, use the deployed database. Otherwise use the local newsscraper database
 var MONGODB_URI = process.env.MONGODB_URI || "mongodb://localhost/newsscraper";
 
 // Set mongoose to leverage built in JavaScript ES6 Promises
@@ -61,7 +35,6 @@ mongoose.Promise = Promise;
 mongoose.connect(MONGODB_URI, {
   useMongoClient: true
 });
-
 
 //Parse application/x-www-form-urlencoded
 app.use(bodyParser.urlencoded({ extended: false }));
